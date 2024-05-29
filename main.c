bcd25fe1c23ae02b08b711d081a88e14473dcb01
bartgo <150788413+bartgogo@users.noreply.github.com>
2024-05-29 17:03:24 +0800

    ddd

--- main.c
+++ main.c
@@ -140,6 +140,7 @@ int sendFileToLinux(const char *filename, PORT com_port) {
     }
     SendData(com_port,&b,1);
     // 发送文件名到 Linux 开发板
+    Sleep(1000);
     bytes_sent = SendData(com_port, filename, strlen(filename));
     if (bytes_sent <= 0) {
         printf("Failed to send filename.\n");
