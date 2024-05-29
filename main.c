ecf275185296c7a5d13375416dcc298ed44608f8
bartgo <150788413+bartgogo@users.noreply.github.com>
2024-05-29 17:14:09 +0800

    ddd

--- main.c
+++ main.c
@@ -134,7 +134,7 @@ int sendFileToLinux(const char *filename, PORT com_port) {
     char b='b';
     printf("专为丢包设置");
     char x='1';
-    for(int i=0;i<500;i++) {
+    for(int i=0;i<100;i++) {
         SendData(com_port,&x,1);
         Sleep(1);
     }
